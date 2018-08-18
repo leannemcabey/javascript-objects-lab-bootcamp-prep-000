3b7c252b7fa60e15ba481d52cdc1f1fdc12dd884
Leanne Cabey <leanne.m.cabey@gmail.com>
2018-08-18 20:25:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {'eggs': 3, "milk": "1 cup"}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  recipesCopy = Object.assign({}, recipes, {key: value})
+  return Object.assign({}, recipes, {key: value})
 }
